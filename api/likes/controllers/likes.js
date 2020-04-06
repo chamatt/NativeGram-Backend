28ed44cb71567637eecf367eca45181576bf0746
chamatt <matt.vicent@gmail.com>
2020-04-06 03:09:13 -0300

    add custom likes create logic

--- api/likes/controllers/likes.js
+++ api/likes/controllers/likes.js
@@ -1,8 +1,36 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
+
+module.exports = {
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
+
+  async create(ctx) {
+    let entity;
+
+    const [likes] = await strapi.services.likes.find({
+      "user.id": ctx.state.user.id,
+    });
+
+    if (likes) {
+      return ctx.unauthorized(`You have already liked this`);
+    }
+
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.likes.create(data, { files });
+    } else {
+      entity = await strapi.services.likes.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.likes });
+  },
+};
