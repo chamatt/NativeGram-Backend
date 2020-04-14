6526ee902af8976e4bbea09ae11f26061e9cdaf7
chamatt <matt.vicent@gmail.com>
2020-04-14 20:59:54 -0300

    follow relationships

---
+++ web-build/precache-manifest.a72be7b05d93f795fbe4fd398ebf2918.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9462a42574816580cd23",
+    "url": "/static/js/app.0ca274dd.chunk.js"
+  },
+  {
+    "revision": "dc6d7c46a725d6866bcd",
+    "url": "/static/js/runtime~app.44ddfa1e.js"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "9b76284573239dd8b6ac7b0ae75744a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38c43d75eeea8e599d42b7b8975fc72e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "04cf1ae10e30155ca207400239ec1cbd",
+    "url": "/register-service-worker.js"
+  }
+];
