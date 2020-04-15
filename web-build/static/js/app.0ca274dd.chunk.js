9f0e1651e89baa36bc439aff030d12108e43e778
chamatt <matt.vicent@gmail.com>
2020-04-14 21:01:46 -0300

    delete web-build

--- web-build/static/js/app.0ca274dd.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonp=this.webpackJsonp||[]).push([[0],[function(e,o,n){e.exports=n(1)},function(e,o){"serviceWorker"in navigator&&window.addEventListener("load",(function(){navigator.serviceWorker.register("/expo-service-worker.js",{scope:"/"}).then((function(e){})).catch((function(e){console.info("Failed to register service-worker",e)}))}))}],[[0,1]]]);
-//# sourceMappingURL=app.0ca274dd.chunk.js.map
